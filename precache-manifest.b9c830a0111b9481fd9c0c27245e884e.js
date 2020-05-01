569a57e39df60408b460a1bea2867a1290c6e01a
Kevin Hernandez <kevin.hernandez1802@gmail.com>
2020-05-01 15:39:28 -0500

    Updates

---
+++ precache-manifest.b9c830a0111b9481fd9c0c27245e884e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "460e46e0d3c4ff990b6a97d3d6e4e827",
+    "url": "/matchfire-assesment/index.html"
+  },
+  {
+    "revision": "3d524de4c2a355781714",
+    "url": "/matchfire-assesment/static/css/main.6c25c439.chunk.css"
+  },
+  {
+    "revision": "5deedd8456eaf6a4c98e",
+    "url": "/matchfire-assesment/static/js/2.468b2361.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/matchfire-assesment/static/js/2.468b2361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d524de4c2a355781714",
+    "url": "/matchfire-assesment/static/js/main.c8354a51.chunk.js"
+  },
+  {
+    "revision": "f99489f4978a9ced035c",
+    "url": "/matchfire-assesment/static/js/runtime-main.23bcb7f1.js"
+  }
+]);
